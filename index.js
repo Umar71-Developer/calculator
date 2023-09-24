632199d11861d880de8022784a9a8f06f0d006b7
Umar71-Developer <umarhayyat710000@gmail.com>
2023-09-24 16:15:55 +0500

    abc

---
+++ index.js
@@ -0,0 +1,38 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([{
+        type: "number",
+        name: "numberone",
+        message: "kindly enter your first no."
+    },
+    {
+        type: "number",
+        name: "numbertwo",
+        message: "kindly enter your second no."
+    },
+    {
+        type: "list",
+        name: "operator",
+        choices: ["*", "+", "-", "/"],
+        message: "kindly select operator."
+    }
+]);
+const { numberone, numbertwo, operator } = answer;
+if (numberone && numbertwo && operator) {
+    let result = 0;
+    if (operator == "+") {
+        result = numberone + numbertwo;
+    }
+    else if (operator == "-") {
+        result = numberone - numbertwo;
+    }
+    else if (operator == "*") {
+        result = numberone * numbertwo;
+    }
+    else if (operator == "/") {
+        result = numberone / numbertwo;
+    }
+    console.log("your result is:", result);
+}
+else {
+    console.log("Please enter a valid no");
+}
